a2423185b5359a76485338aa671f224280f51f81
Rich Chamberlain <richarddchamberlain@gmail.com>
2019-09-20 13:59:49 +0100

    Removed and simplified nesting

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -14,40 +14,39 @@ function update_quality() {
     if (items[i].name === SULFURAS) {
       continue;
     }
-    if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASSES) {
-      if (items[i].quality > 0) {
-        items[i].quality = items[i].quality - 1;
+
+    if (items[i].name === BACKSTAGE_PASSES) {
+      if(items[i].quality < 50) {
+        if (items[i].sell_in > 10 && items[i].sell_in < 50) {
+          items[i].quality += 1;
+        } else if (items[i].sell_in > 5 && items[i].sell_in <= 10) {
+          items[i].quality += 2;
+        } else if (items[i].sell_in <= 5) {
+          items[i].quality += 3;
+        }
+      }
+    } else if (items[i].name === AGED_BRIE) {
+      if (items[i].quality < 50) {
+        items[i].quality += 1;
       }
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1;
-        if (items[i].name == BACKSTAGE_PASSES) {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-        }
+      if (items[i].quality > 0) {
+        items[i].quality -= 1;
       }
     }
+
     items[i].sell_in = items[i].sell_in - 1;
+
     if (items[i].sell_in < 0) {
-      if (items[i].name != AGED_BRIE) {
-        if (items[i].name != BACKSTAGE_PASSES) {
-          if (items[i].quality > 0) {
-            items[i].quality = items[i].quality - 1;
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality;
+      if (items[i].name === BACKSTAGE_PASSES) {
+        items[i].quality = 0;
+      } else if (items[i].name === AGED_BRIE) {
+        if (items[i].quality < 50) {
+          items[i].quality += 1;
         }
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
+        if (items[i].quality > 0) {
+          items[i].quality -= 1;
         }
       }
     }
