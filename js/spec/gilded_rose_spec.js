fac7f91f431d47f7a16175c9a94167926895fead
Rich Chamberlain <richarddchamberlain@gmail.com>
2019-09-20 11:49:01 +0100

    working test

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,9 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("reduces the quality by 1 every day", function() {
+    items = [ new Item("foo", 3, 50) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].quality).toEqual(49);
   });
 
 });
