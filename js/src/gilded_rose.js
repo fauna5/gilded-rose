9115cd9be74460beed9062b11037ef1cea647a9f
Rich Chamberlain <richarddchamberlain@gmail.com>
2019-09-20 13:30:03 +0100

    removed sulfuras

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -11,46 +11,43 @@ var SULFURAS = 'Sulfuras, Hand of Ragnaros';
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
+    if (items[i].name === SULFURAS) {
+      continue;
+    }
     if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASSES) {
       if (items[i].quality > 0) {
-        if (items[i].name != SULFURAS) {
-          items[i].quality = items[i].quality - 1
-        }
+        items[i].quality = items[i].quality - 1;
       }
     } else {
       if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
+        items[i].quality = items[i].quality + 1;
         if (items[i].name == BACKSTAGE_PASSES) {
           if (items[i].sell_in < 11) {
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality = items[i].quality + 1;
             }
           }
           if (items[i].sell_in < 6) {
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality = items[i].quality + 1;
             }
           }
         }
       }
     }
-    if (items[i].name != SULFURAS) {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
+    items[i].sell_in = items[i].sell_in - 1;
     if (items[i].sell_in < 0) {
       if (items[i].name != AGED_BRIE) {
         if (items[i].name != BACKSTAGE_PASSES) {
           if (items[i].quality > 0) {
-            if (items[i].name != SULFURAS) {
-              items[i].quality = items[i].quality - 1
-            }
+            items[i].quality = items[i].quality - 1;
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          items[i].quality = items[i].quality - items[i].quality;
         }
       } else {
         if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+          items[i].quality = items[i].quality + 1;
         }
       }
     }
