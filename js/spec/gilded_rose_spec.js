ca936272d3f8b028d7579daa25af4a9a70d0ab2e
Rich Chamberlain <richarddchamberlain@gmail.com>
2019-09-20 11:57:35 +0100

    Added test for sell_in

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -6,4 +6,9 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(49);
   });
 
+  it("reduces the sellin by 1 every day", function() {
+    items = [ new Item("foo", 3, 50) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(2);
+  });
 });
