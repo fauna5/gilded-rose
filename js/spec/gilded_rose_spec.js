4bc35c6b6eb6b731b4e67a12fab812b48c0c5edb
Rich Chamberlain <richarddchamberlain@gmail.com>
2019-09-20 12:36:40 +0100

    Final tests for sell in less than zero

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,25 +1,53 @@
 describe('Gilded Rose', function() {
-  it('reduces the quality by 1 every day', function() {
-    items = [new Item('foo', 3, 50)];
-    update_quality();
-    expect(items[0].quality).toEqual(49);
-  });
-
   it('reduces the sellin by 1 every day', function() {
     items = [new Item('foo', 3, 50)];
     update_quality();
     expect(items[0].sell_in).toEqual(2);
   });
 
+  it('reduces the quality by 1 every day', function() {
+    items = [new Item('foo', 3, 50)];
+    update_quality();
+    expect(items[0].quality).toEqual(49);
+  });
+
+  it('reduces the quality by 2 every day after sell_in date passes', function() {
+    items = [new Item('foo', -2, 50)];
+    update_quality();
+    expect(items[0].quality).toEqual(48);
+  });
+
+  it('does not reduce the quality if if reaches zero', function() {
+    items = [new Item('foo', 3, 0)];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+
+  it('does not reduce the quality if if reaches zero even after sell in passes', function() {
+    items = [new Item('foo', -1, 0)];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+
   describe('Special products', () => {
     describe(AGED_BRIE, () => {
-      it('increases the quality if Brie and quality < 50', function() {
+      it('increases the quality by 1 if quality < 50s', function() {
         items = [new Item(AGED_BRIE, 3, 30)];
         update_quality();
         expect(items[0].quality).toEqual(31);
       });
-      it('does not change the quality if Brie and quality > 50', function() {
+      it('increases the quality by 2 if quality < 50 and sell in date pasees', function() {
+        items = [new Item(AGED_BRIE, 3, 30)];
+        update_quality();
+        expect(items[0].quality).toEqual(31);
+      });
+      it('does not change the quality if quality > 50', function() {
         items = [new Item(AGED_BRIE, 3, 100)];
+        update_quality();
+        expect(items[0].quality).toEqual(100);
+      });
+      it('does not change the quality if quality > 50 and sell in date passes', function() {
+        items = [new Item(AGED_BRIE, -1, 100)];
         update_quality();
         expect(items[0].quality).toEqual(100);
       });
@@ -47,10 +75,20 @@ describe('Gilded Rose', function() {
           expect(items[0].quality).toEqual(33);
         });
       });
+      it('the quality goes to zero after sell in date passes', function() {
+        items = [new Item(BACKSTAGE_PASSES, -1, 100)];
+        update_quality();
+        expect(items[0].quality).toEqual(0);
+      });
     });
     describe(SULFURAS, () => {
       it('does not change quality of Sulfuras', function() {
         items = [new Item(SULFURAS, 20, 100)];
+        update_quality();
+        expect(items[0].quality).toEqual(100);
+      });
+      it('does not change quality of Sulfuras even after sell in passes', function() {
+        items = [new Item(SULFURAS, -1, 100)];
         update_quality();
         expect(items[0].quality).toEqual(100);
       });
